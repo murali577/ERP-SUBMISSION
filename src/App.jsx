4f00dd55244bde84a49d429aec4bc0b9994d78ca
murali577 <114055409+murali577@users.noreply.github.com>
2024-03-13 10:08:00 +0000

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import DashboardComponent from "./DashboardComponent";
+import OrderCalenderView from "./OrderCalendarView";
+import OrderManagementComponent from "./OrderManagementComponent";
+import ProductManagementComponent from "./ProductManagementComponent";
+
+function App() {
+
+
+  return (
+    <>
+      <DashboardComponent/>
+      <OrderCalenderView/>
+      <OrderManagementComponent/>
+      <ProductManagementComponent/>
+    </>
+  )
+}
+
+export default App
